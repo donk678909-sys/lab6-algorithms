b7c41b9607ab0f1d8941a523874d1a40f35d9caa
donk678909-sys <donk678909@gmail.com>
2025-11-13 09:37:59 +0500

    Add files via upload

---
+++ лабораторная 6/bubble_sort.py
@@ -0,0 +1,24 @@
+def bubble_sort(arr):
+	n = len(arr)
+	for i in range(n):
+	    for j in range(0, n - i - 1):
+		    if arr[j] > arr[j + 1]:
+			arr[j], arr[j + 1] = arr[j + 1], arr[j]
+	return arr
+
+# Тестирование алгоритма
+print("=== Тест 1 ===")
+test1 = [5, 2, 9, 1, 5, 6]
+print("До сортировки:", test1)
+print("После сортировки:", bubble_sort(test1.copy()))
+
+print("=== Тест 2 ===")
+test2 = [3, 0, -1, 8, 7]
+print("До сортировки:", test1)
+print("После сортировки:", bubble_sort(test2.copy()))
+
+print("=== Тест 3 ===")
+test1 = [1]
+print("До сортировки:", test1)
+print("После сортировки:", bubble_sort(test3.copy()))
+
